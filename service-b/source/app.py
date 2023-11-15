d8d5112e69439dafe419bf27c175e6a7df964792
Nantawat Chairat <nantawat@opsta.co.th>
2023-11-15 22:48:08 +0700

    init

---
+++ service-b/source/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/api/data", methods=["POST"])
+def APIDATA():
+    datetime_now = datetime.datetime.now()
+    print(str(request.data))
+    print(datetime_now.strftime("%Y-%m-%d %H:%M:%S"))
+    return "ok"
